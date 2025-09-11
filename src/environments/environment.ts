7af55b9eaeffecee6b78ff3d2de2e00bc2ae7368
Dzmitry Samuilik <dzmitry_samuilik@epam.com>
2025-09-11 17:32:33 +0300

    Add environment files to .gitignore and remove from tracking

--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://script.google.com/macros/s/AKfycbzw1xhYGetrRTJSBmTPA5bBoNOartY21JXujyrtErR3VDn_pRkCuSeOl1RhNYbZrj18/exec',//'${API_URL_DEV}'
-};
