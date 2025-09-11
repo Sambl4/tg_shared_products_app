7f896b592e4cb364b399b5ddcc9994083dc87c36
Dzmitry Samuilik <dzmitry_samuilik@epam.com>
2025-09-11 15:47:00 +0300

    Add environment variables and GitHub Actions workflow for secure GAS API deployment

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, resource } from '@angular/core';
 import { TelegramService } from '../../services/telegram.service';
 import { ProductGroup, ProductService } from '../../services/product.service';
 import { ProductListComponent } from '../../components/product-list.component/product-list.component';
 import { Router } from '@angular/router';
-import { FeedbackComponent } from '../feedback/feedback.component';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-shop',
@@ -16,17 +16,30 @@ export class ShopComponent {
     telegram = inject(TelegramService);
     products = inject(ProductService);
     productGroupsName = Object.values(ProductGroup);
-    feedback = FeedbackComponent.prototype.feedback;
+
+
+    apiData = resource({
+        loader: () => {
+            return fetch(environment.apiUrl).then(res => res.json())
+        }
+    })
 
     constructor(
         private router: Router,
     ) {
         this.telegram.MainButton.setText('Start Order');
         this.telegram.MainButton.show();
-        console.log(this.feedback())
     }
 
     goToFeedback() {
         this.router.navigate(['/feedback']);
+    }
+
+    callGas() {
+        console.log(this.apiData.value())
+    }
+
+    callSpreadsheet() {
+        console.log('')
     }
 }
